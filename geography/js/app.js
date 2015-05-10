2de470b25940ec803d167f663c70828d34d343e4
Dan Serban <dserban01@yahoo.com>
2015-05-10 19:28:59 +0300

    Add content

---
+++ geography/js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var app = angular.module('AngularGeographyApp', []);
+
+app.controller('AngularGeographyController', ['$scope', function($scope) {
+  // begin
+
+
+
+  // end
+
+  window.sc = $scope;
+
+}]);
+
---
+++ traffic_light/js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var app = angular.module('AngularTrafficLightApp', []);
+
+app.controller('AngularTrafficLightController', ['$scope', '$interval', function($scope, $interval) {
+  // begin
+
+
+
+  // end
+
+  window.sc = $scope;
+
+}]);
+
