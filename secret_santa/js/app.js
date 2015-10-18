e8793db5238cde04b91ad138fa0998ffbf065425
MIHAI POPESCU <mihai.ppscu@gmail.com>
2015-10-18 15:40:51 +0300

    message

--- secret_santa/js/app.js
+++ secret_santa/js/app.js
@@ -2,10 +2,50 @@
 
 var app = angular.module('SecretSantaNameDrawingApp', []);
 
-app.controller('SecretSantaNameDrawingController', ['$scope', function($scope) {
+app.controller('SecretSantaNameDrawingController', ['$scope', '$timeout', function($scope, $timeout) {
   // begin
+  $scope.currentlyViewing = 'inputPage';
+  $scope.commaSeparatedListOfNames = 'Alice, Andy, Ashley, Betty, Billy, Bobby, Bradley, Chris, Craig, Dorothy, Emily, Gregory, Jack, Jason, Jennifer, Jessica, Linda, Maggie, Ryan, Tony';
+  $scope.personWhoseNameWasLastClicked= '';
 
+  function generateSecretSantaHash(commaSeparatedListOfNames) {
+    var resultOfSplittingByComma = commaSeparatedListOfNames.split(',');
+    function trimAsFirstClassFunction(element) {
+      return element.trim();
+    }
+    var resultOfTrimmingWhitespace = resultOfSplittingByComma.map(trimAsFirstClassFunction);
+    var santas = _.shuffle(resultOfTrimmingWhitespace);
+    var giftRecipients = _.clone(santas);
+    var rightmostElement = santas.pop();
+    var leftmostElement = giftRecipients.shift();
+    var resultOfZip = _.zip(santas, giftRecipients);
+    function absorbTupleIntoHash (hash, tuple) {
+      hash[tuple[0]] = tuple[1];
+      return hash;
+    }
+    var initialSecretSantaHash = {};
+    initialSecretSantaHash[rightmostElement] = leftmostElement;
+    var secretSantaHash = _.reduce(resultOfZip, absorbTupleIntoHash, initialSecretSantaHash);
+    return secretSantaHash;
+  }
 
+  function drawNames(names) {
+      $scope.secretSantaHash = generateSecretSantaHash(names);
+      $scope.currentlyViewing = 'outputPage';
+  }
+
+  function resetPerson() {
+      $scope.personWhoseNameWasLastClicked = '';
+  }
+
+  function setPersonTo(person) {
+      $scope.personWhoseNameWasLastClicked = person;
+
+      $timeout(resetPerson, 500);
+  }
+
+  $scope.setPersonTo = setPersonTo;
+  $scope.drawNames = drawNames;
 
   // end
 
@@ -37,4 +77,3 @@ app.controller('SecretSantaNameDrawingController', ['$scope', function($scope) {
     return secretSantaHash;
   }
 */
-
--- traffic_light/js/app.js
+++ traffic_light/js/app.js
@@ -4,12 +4,33 @@ var app = angular.module('AngularTrafficLightApp', []);
 
 app.controller('AngularTrafficLightController', ['$scope', '$interval', function($scope, $interval) {
   // begin
+  $scope.trafficLightState = 'dark';
+
+  var transitions =  {
+      'dark' : 'red',
+      'red' : 'green',
+      'green' : 'amber',
+      'amber' : 'red'
+  }
 
 
+  function setTrafficLightStateTo(color) {
+      $scope.trafficLightState = color;
+  }
+
+  function advance() {
+      $scope.trafficLightState = transitions[$scope.trafficLightState];
+  }
+
+
+  // asta e un alias ca functia sa fie vazuta in consola si din html
+  $scope.setTrafficLightStateTo = setTrafficLightStateTo;
+  $scope.advance = advance;
+
+  $interval(advance, 500);
 
   // end
 
   window.sc = $scope;
 
 }]);
-
